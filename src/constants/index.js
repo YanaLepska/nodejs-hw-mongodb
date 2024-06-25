ccbb6d59e35090134b16b265f56e4a4049b2e0dd
Yana Lepska <lepskayana1624@gmail.com>
2024-06-25 14:51:25 +0300

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,7 +23,7 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temps');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-CLOUD_NAME:'CLOUD_NAME',
-API_KEY:'API_KEY',
-API_SECRET:'API_SECRET',
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
 };
--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -3,10 +3,10 @@ import { env } from './env.js';
 import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
-    secure: true,
-    cloud_name: env(CLOUDINARY.CLOUD_NAME),
-    api_key: env(CLOUDINARY.API_KEY),
-    api_secret: env(CLOUDINARY.API_SECRET),
+  secure: true,
+  cloud_name: env(CLOUDINARY.CLOUD_NAME),
+  api_key: env(CLOUDINARY.API_KEY),
+  api_secret: env(CLOUDINARY.API_SECRET),
 });
 
 export const saveFileToCloudinary = async (file) => {
